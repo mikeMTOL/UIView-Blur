ca8e1605708764947097cf9a9fc09719bad02bc5
mike <mmusallam@gmail.com>
2014-05-22 01:23:42 -0400

    minor fix, removed testUnit target

--- testBlurredUIView/KSAppDelegate.h
+++ testBlurredUIView/KSAppDelegate.h
@@ -3,7 +3,7 @@
 //  testBlurredUIView
 //
 //  Created by salah on 2014-05-21.
-//  Copyright (c) 2014 kronositi inc. All rights reserved.
+//  Copyright (c) 2014 Mike. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
